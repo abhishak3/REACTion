627bc174e4ab68feabd965c916d1f76f72837fe6
abhishak3 <abhishek.pandey.3237@gmail.com>
2022-08-11 11:35:41 +0530

    index vs object keys

--- 02.React_Hello_world/hacker-stories/src/App.js
+++ 02.React_Hello_world/hacker-stories/src/App.js
@@ -1,8 +1,24 @@
-//const name = "GodZilla";
-
 function say(message) {
     return `Me: ${message}`;
 }
+
+const details = [
+    {
+        'id': 1,
+        'name': 'Abhishek',
+        'age': 32
+    },
+    {
+        'id': 2,
+        'name': 'Sunny',
+        'age': 18
+    },
+    {
+        'id': 3,
+        'name': 'Honey',
+        'age': 19
+    }
+]
 
 function App() {
     return (
@@ -11,6 +27,19 @@ function App() {
 
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" />
+
+            <hr />
+
+            {details.map((item, index) => {
+                return (
+                    <div key={index}>{item.name} is {item.age} years old.</div>
+                    // avoid using `index` because if list reorders, every key will
+                    // change and React's reconciler have to redraw the array which
+                    // can cause significant performance loss.
+                    // use key associated with object. It will be more stable.
+                )
+            })}
+
         </div>
     );
 }
